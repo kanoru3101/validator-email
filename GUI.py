e1cd6dbb2adf9317518705e57b3f08ddc056ed95
Victor <victor@abcsite.net>
2018-02-13 18:45:27 +0200

    add two image true and false. Fixed class Validator in validator.py, add item_frame

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication, QMessageBox, QDesktopWidget, QMainWindow,
     QGridLayout, QLabel, QLineEdit, QTextEdit, QScrollBar, QFrame, QGroupBox, QVBoxLayout, QHBoxLayout, QMenuBar,
     QAction, QFileDialog)
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import * # QCoreApplication  #для сигналів
 from validator import *
 import csv
@@ -54,16 +54,112 @@ class Application(QMainWindow):
         return frame_history
 
 
-    def item_frame(self):
+    def update_item_img(self, erorr_list):
         """
-
         :return:
         """
+        self.email_syntax_img.setPixmap(self.true_img)
+        self.disposable_domain_img.setPixmap(self.true_img)
+        self.isp_syntax_img.setPixmap(self.true_img)
+        self.domain_validity_img.setPixmap(self.true_img)
+        self.mail_exchanger_img.setPixmap(self.true_img)
+        self.smpt_connection_img.setPixmap(self.true_img)
+        self.mailbox_existence_img.setPixmap(self.true_img)
+        if erorr_list is not None:
+            for error in erorr_list:
+                if error == "Email Syntax":
+                    self.email_syntax_img.setPixmap(self.false_img)
+                if error == "Known Disposable Email Domain":
+                    self.disposable_domain_img.setPixmap(self.false_img)
+                if error == "ISP-Specific Syntax":
+                    self.isp_syntax_img.setPixmap(self.false_img)
+                if error == "Domain Validity":
+                    self.domain_validity_img.setPixmap(self.false_img)
+                if error == "Mail Exchanger (MX) records":
+                    self.mail_exchanger_img.setPixmap(self.false_img)
+                if error == "SMTP Connection":
+                    self.smpt_connection_img.setPixmap(self.false_img)
+                if error == "Mailbox Existence":
+                    self.mailbox_existence_img.setPixmap(self.false_img)
 
-        frame_item = QFrame()
-        frame_item.setStyleSheet("background-color: rgb(200, 255, 255)")
-        frame_item.setVisible(True)  #Visible frame
-        return frame_item
+    def item_frame(self):
+        """
+        Creating item_frame
+        :return:
+        """
+        self.true_img = QPixmap("true.png")
+        self.true_img = self.true_img.scaled(64, 50)
+        self.false_img = QPixmap("false.png")
+        self.false_img = self.false_img.scaled(64, 50)
+
+        self.test_img = QPixmap("logo.png")
+        self.test_img = self.test_img.scaled(64, 50)
+
+        email_syntax = QLabel("Email Syntax:")
+        self.email_syntax_img = QLabel()
+        self.email_syntax_img.setPixmap(self.true_img)
+        email_syntax_loyout = QHBoxLayout()
+        email_syntax_loyout.addWidget(email_syntax)
+        email_syntax_loyout.addWidget(self.email_syntax_img)
+
+        disposable_domain = QLabel("Known Disposable Email Domain:")
+        self.disposable_domain_img = QLabel()
+        self.disposable_domain_img.setPixmap(self.false_img)
+        disposable_domain_layout = QHBoxLayout()
+        disposable_domain_layout.addWidget(disposable_domain)
+        disposable_domain_layout.addWidget(self.disposable_domain_img)
+
+        isp_syntax = QLabel("ISP-Specific Syntax:")
+        self.isp_syntax_img = QLabel()
+        self.isp_syntax_img.setPixmap(self.test_img)
+        isp_syntax_layout = QHBoxLayout()
+        isp_syntax_layout.addWidget(isp_syntax)
+        isp_syntax_layout.addWidget(self.isp_syntax_img)
+
+        domain_validity = QLabel("Domain Validity:")
+        self.domain_validity_img = QLabel()
+        self.domain_validity_img.setPixmap(self.test_img)
+        domain_validity_layout = QHBoxLayout()
+        domain_validity_layout.addWidget(domain_validity)
+        domain_validity_layout.addWidget(self.domain_validity_img)
+
+        mail_exchanger = QLabel("Mail Exchanger (MX) records:")
+        self.mail_exchanger_img = QLabel()
+        self.mail_exchanger_img.setPixmap(self.test_img)
+        mail_exchanger_layout = QHBoxLayout()
+        mail_exchanger_layout.addWidget(mail_exchanger)
+        mail_exchanger_layout.addWidget(self.mail_exchanger_img)
+
+        smpt_connection = QLabel("SMTP Connection:")
+        self.smpt_connection_img = QLabel()
+        self.smpt_connection_img.setPixmap(self.test_img)
+        smpt_connection_layout = QHBoxLayout()
+        smpt_connection_layout.addWidget(smpt_connection)
+        smpt_connection_layout.addWidget(self.smpt_connection_img)
+
+        mailbox_existence = QLabel("Mailbox Existence:")
+        self.mailbox_existence_img = QLabel()
+        self.mailbox_existence_img.setPixmap(self.test_img)
+        mailbox_existence_layout = QHBoxLayout()
+        mailbox_existence_layout.addWidget(mailbox_existence)
+        mailbox_existence_layout.addWidget(self.mailbox_existence_img)
+
+        frame_layout = QVBoxLayout()
+        frame_layout.addLayout(email_syntax_loyout)
+        frame_layout.addLayout(disposable_domain_layout)
+        frame_layout.addLayout(isp_syntax_layout)
+        frame_layout.addLayout(domain_validity_layout)
+        frame_layout.addLayout(mail_exchanger_layout)
+        frame_layout.addLayout(smpt_connection_layout)
+        frame_layout.addLayout(mailbox_existence_layout)
+
+        #Create Main Frame for email item
+        self.frame_item = QFrame()
+        self.frame_item.setLayout(frame_layout)
+        self.frame_item.setVisible(False)
+        self.frame_item.setStyleSheet("background-color: rgb(200, 255, 255)")
+
+          #Visible frame
 
     def central_widget(self):
         """
@@ -71,28 +167,68 @@ class Application(QMainWindow):
 
         :return:
         """
-        label_email = QLabel("Enter Email:")
-        line_email = QLineEdit()
-        btn_validator = QPushButton('Valid')
-        status_email = QLabel("Status Email:")
+        self.label_email = QLabel("Enter Email:")
+        self.line_email = QLineEdit()
+        self.btn_validator = QPushButton('Valid')
+        self.status_email = QLabel("Status Email:")
         self.is_valid =  QLabel("Empty status")
 
-        frame_item = self.item_frame()
-        frame_history = self.history_frame()
+        self.item_frame()
+        self.frame_history = self.history_frame()
 
         grid = QGridLayout()
         grid.setSpacing(10)
-        grid.addWidget(label_email, 0, 0)
-        grid.addWidget(line_email, 1, 0, 1, 3)
-        grid.addWidget(btn_validator, 1, 3)
-        grid.addWidget(status_email, 2, 0)
-        grid.addWidget(frame_item, 3, 0, 7, 6)
+        grid.addWidget(self.label_email, 0, 0)
+        grid.addWidget(self.line_email, 1, 0, 1, 3)
+        grid.addWidget(self.btn_validator, 1, 3)
+        grid.addWidget(self.status_email, 2, 0)
+        grid.addWidget(self.frame_item, 3, 0, 6, 6)
         grid.addWidget(self.is_valid, 2, 1)
-        grid.addWidget(frame_history,0, 6, 10, 10)
+        grid.addWidget(self.frame_history,0, 6, 10, 10)
 
         window = QWidget(self)
         window.setLayout(grid)
+
         self.setCentralWidget(window)
+        self.btn_validator.clicked.connect(self.buttonClickedValid)
+
+
+    def buttonClickedValid(self):
+        """
+        :return:
+        """
+        if self.line_email.text().strip() is not None and self.line_email.text().strip() != "":
+            self.emailValidator(self.line_email.text().strip())
+
+
+    def emailValidator(self, email):
+        """
+        :param email:
+        :return:
+        """
+        self.validator.setEmail(email)
+        self.validator.find_email_at_site()
+        result = self.validator.get_result()
+        print(result)
+        if result == "The Email is valid":
+            self.is_valid.setText(email + " " + result)
+            self.is_valid.setStyleSheet("color: green")
+            self.frame_item.setVisible(False)
+
+        elif result == "The email is not valid":
+            self.is_valid.setText(email + " " + result)
+            self.is_valid.setStyleSheet("color: red")
+
+            errors = self.validator.get_error_email()
+            #print(errors)
+            self.update_item_img(errors)
+            self.frame_item.setVisible(True)
+        else:
+            self.is_valid.setText("Error, Try again")
+            self.is_valid.setStyleSheet("color: yellow")
+            self.frame_item.setVisible(False)
+
+
 
 
     def center_window(self):
@@ -147,6 +283,7 @@ class Application(QMainWindow):
         reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No,
                                      QMessageBox.No)
         if reply == QMessageBox.Yes:
+            self.validator.close()
             event.accept()
         else:
             event.ignore()
@@ -162,6 +299,7 @@ class Application(QMainWindow):
         exitAction = QAction(QIcon('exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.validator.close)
         exitAction.triggered.connect(app.quit)
 
         info_program = QAction(QIcon(''), '&Information', self)
@@ -176,6 +314,7 @@ class Application(QMainWindow):
 
     def initUI(self):
 
+        self.validator = Validator()
         self.menu_bar()
         self.central_widget()
         self.center_window()
--- validator.py
+++ validator.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.by import By
 
 class Formater():
 
-
     def findFormat(str):
         """
         find format file from way
@@ -27,19 +26,23 @@ class Formater():
     def dataValidator(data,format):
         if format == "csv":
             pass
-
         if format == "json":
             pass
         if format == "txt":
             pass
 
+
 class Validator():
 
-    def __init__(self, e):
-        self.email = e
+    def __init__(self):
+        #self.email = e
         #self.driver = d
         #self.is_alert = None
         self.error_email = []
+        self.driver = webdriver.Firefox(executable_path='C:\\geckodriver_firefox\\\geckodriver.exe')
+
+    def setEmail(self, e):
+        self.email = e
 
     def get_result(self):
         """
@@ -53,7 +56,6 @@ class Validator():
         else:
             raise Exception("Error while executing")
 
-
     def get_error_email(self):
         """
         Items that do not match the email address
@@ -66,22 +68,21 @@ class Validator():
 
     def grabber(self):
         if self.is_alert == True:
-            page = driver.page_source
+            page = self.driver.page_source
             soup = BeautifulSoup(page, 'lxml')
             self.result = soup.find('div', id='ValidationResponse').find('div', class_='alert alert-success').get_text()
         if self.is_alert == False:
-            page = driver.page_source
+            page = self.driver.page_source
             soup = BeautifulSoup(page, 'lxml')
             self.result = soup.find('div', id='ValidationResponse').find('div', class_='alert alert-danger').get_text()
             dangers = soup.find_all('li', class_='list-group-item')
             for item in dangers:
                 if item.find('i', class_='icon-remove'):
                     error = item.get_text()
-                    print(error)
                     self.error_email.append(error[:-3])
 
 
-    def find_email_at_site(self, drive):
+    def find_email_at_site(self):
         """
         search email at site http://www.emailvalidator.co
         :return:
@@ -89,27 +90,26 @@ class Validator():
         r = requests.get("http://www.emailvalidator.co")
         if r.status_code != 200:
             raise r.status_code
-
-        driver.get("http://www.emailvalidator.co")
-        label_search = WebDriverWait(driver, 10).until(
+        self.driver.get("http://www.emailvalidator.co")
+        label_search = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "EmailAddress"))
         )
         label_search.clear()
         label_search.send_keys(self.email)
-        buttom = WebDriverWait(driver, 10).until(
+        buttom = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'button.yellow.large'))
         )
         buttom.click()
-        WebDriverWait(driver, 5)
+        WebDriverWait(self.driver, 5)
         is_alert = None
         try:
-            alert = WebDriverWait(driver, 30).until(
+            alert = WebDriverWait(self.driver, 5).until(
                 EC.presence_of_element_located((By.CLASS_NAME, 'alert.alert-success'))
             )
             self.is_alert = True
         except:
             try:
-                alert = WebDriverWait(driver, 30).until(
+                alert = WebDriverWait(self.driver, 5).until(
                     EC.presence_of_element_located((By.CLASS_NAME, 'alert.alert-danger'))
                 )
                 self.is_alert = False
@@ -118,9 +118,23 @@ class Validator():
         finally:
             self.grabber()
 
+
+    def close(self):
+        """
+        close webdriver
+        :return:
+        """
+        self.driver.close()
+
+
+
+
 """
 if __name__ == '__main__':
 
+    app = Validator("kanoru3101@gmail.com")
+    app.find_email_at_site()
+    print(app.get_result())
     #driver.get("http://www.emailvalidator.co")
     #email_validator(driver)
     driver = webdriver.Firefox(executable_path='C:\\geckodriver_firefox\\\geckodriver.exe')
@@ -132,5 +146,5 @@ if __name__ == '__main__':
     app2.find_email_at_site(driver)
     print(app2.get_result())
     print(app2.get_error_email())
-"""
 
+"""
